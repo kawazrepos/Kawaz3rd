e789819576c1659375f90eea79c7831f0abb2149
Λlisue <lambdalisue@hashnote.net>
2014-04-15 18:24:48 +0900

    Fix runtests.py to add PYTHON_PATH

--- runtests.py
+++ runtests.py
@@ -1,14 +1,23 @@
 import os, sys
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Kawaz.settings'
-test_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, test_dir)
 
-from django.test.utils import get_runner
-from django.conf import settings
+def runtests(verbosity=1, interactive=False):
+    # Add extra PYTHON_PATH
+    root = os.path.abspath(os.path.dirname(__file__))
+    required_paths = [
+            os.path.join(root, 'src'),
+        ]
+    for required_path in required_paths:
+        if required_path not in sys.path:
+            sys.path.insert(0, required_path)
+    # django require DJANGO_SETTINGS_MODULE
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'Kawaz.settings'
 
-def runtests():
+    from django.conf import settings
+    from django.test.utils import get_runner
     TestRunner = get_runner(settings)
-    test_runner = TestRunner(verbosity=1, interactive=True)
+    test_runner = TestRunner(verbosity=verbosity,
+                             interactive=interactive,
+                             failfast=False)
     failures = test_runner.run_tests(['Kawaz'])
     sys.exit(bool(failures))
 
