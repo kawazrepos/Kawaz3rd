39cbcbac136f800b23f5bd5d12d9d34d3d2045cd
giginet <giginet.net@gmail.com>
2014-04-16 02:03:50 +0900

    Implement EntryFactory

---
+++ src/kawaz/apps/blogs/tests/factories.py
@@ -0,0 +1,19 @@
+import factory
+from ..models import Category, Entry
+from kawaz.core.auth.tests.factories import UserFactory
+
+class CategoryFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = Category
+    FACTORY_DJANGO_GET_OR_CREATE = ('label',)
+
+    label = 'イベントレポート'
+    author = factory.SubFactory(UserFactory)
+
+class EntryFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = Entry
+
+    pub_state = 'public'
+    title = '焼肉食べまくる会に参加してきました'
+    body = 'カルビがおいしかった（小並感）'
+    category = factory.SubFactory(CategoryFactory)
+    author = factory.SubFactory(UserFactory)
