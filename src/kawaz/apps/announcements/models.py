17262a6ea282eb02a78599f0dd66da2a410cdbc2
giginet <giginet.net@gmail.com>
2014-04-17 03:10:34 +0900

    Implement Announcement model

---
+++ src/kawaz/apps/announcements/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext as _
+
+from markupfield.fields import MarkupField
+
+User = get_user_model()
+
+class AnnouncementManager(models.Manager):
+    def published(self, request):
+        if request and request.user.is_authenticated():
+            return self.exclude(pub_state='draft')
+        else:
+            return self.filter(pub_state='public')
+    def draft(self, request):
+        if request and request.user.is_authenticated():
+            return self.filter(pub_state='draft', author=request.user)
+        else:
+            return self.none()
+        
+class Announcement(models.Model):
+    """
+    An announcement that came from staff user
+    """
+
+    PUB_STATES = (
+        ('public', _("Public")),
+        ('protected', _("Internal")),
+        ('draft', _("Draft")),
+    )
+    # Required
+    pub_state = models.CharField(_('Publish status'), max_length=10, choices=PUB_STATES)
+    title = models.CharField(_('Title'), max_length=128)
+    body = MarkupField(_('Body'), default_markup_type='markdown')
+    sage = models.BooleanField(_('Silently'), default=False,
+                               help_text=_('If you checked this field. This will not be notified anybody.'))
+    # Uneditable
+    author = models.ForeignKey(User, related_name='created_announcements')
+    created_at = models.DateTimeField(_('Created at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('Modified at'), auto_now=True)
+    objects = AnnouncementManager()
+    
+    class Meta:
+        ordering = ('-created_at',)
+        verbose_name = _('Announcement')
+        verbose_name_plural = _('Announcements')
+        permissions = (
+            ('create_announcement', 'Can create new announcement'),
+            ('change_announcement', 'Can change the announcement'),
+            ('delete_announcement', 'Can delete the announcement'),
+            ('view_announcement', 'Can view the announcement'),
+        )
+
+    def __str__(self):
+        return self.title
+
+from permission.logics import PermissionLogic
+from permission import add_permission_logic
+
+class AnnouncementPermissionLogic(PermissionLogic):
+    """
+    Permission logic which check object publish statement and return
+    whether the user has a permission to see the object
+    """
+    def _has_view_perm(self, user_obj, perm, obj):
+        if obj.pub_state == 'protected':
+            # only authorized user can show protected announcement
+            return user_obj.is_authenticated()
+        if obj.pub_state == 'draft':
+            # only staff user can show draft announcement
+            return user_obj.is_staff()
+        # public
+        return True
+
+    def has_perm(self, user_obj, perm, obj=None):
+        staff_allowed_methods = (
+            'announcements.create_announcement',
+            'announcements.change_announcement',
+            'announcements.delete_announcement',
+        )
+        if perm in staff_allowed_methods and user_obj.is_staff():
+            return True
+        if perm == 'announcements.view_announcement':
+            return self._has_view_perm(user_obj, perm, obj)
+        return False
+add_permission_logic(Announcement, AnnouncementPermissionLogic)
