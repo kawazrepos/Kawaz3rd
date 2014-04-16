994c3874fb75ce3525336670eed46cf36befa43a
giginet <giginet.net@gmail.com>
2014-04-17 03:47:31 +0900

    Add Announcement to admin

--- src/kawaz/apps/announcements/admin.py
+++ src/kawaz/apps/announcements/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Announcement
 
-# Register your models here.
+class AnnouncementAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Announcement, AnnouncementAdmin)
