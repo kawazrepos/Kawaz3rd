2945a842f71d8ddfacc94960fc414072a0a8aa74
giginet <giginet.net@gmail.com>
2014-04-16 17:56:55 +0900

    Revert "Remove MEDIA_ROOT on settings.py"
    This reverts commit 0b7dbe592ab1377a4e2e52a282ec5aa98c793a7a.

--- src/kawaz/settings.py
+++ src/kawaz/settings.py
@@ -99,6 +99,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'static')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
