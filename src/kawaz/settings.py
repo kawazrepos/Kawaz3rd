c1ae42fe18d7bc66066ee0454ad875552cf11a1f
giginet <giginet.net@gmail.com>
2014-04-16 18:32:33 +0900

    Modify storage path

--- src/kawaz/settings.py
+++ src/kawaz/settings.py
@@ -99,7 +99,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'storage')
+MEDIA_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../storage')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
