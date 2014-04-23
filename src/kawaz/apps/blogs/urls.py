09dcb42de778bf956cc2e22dc009754a916576f2
giginet <giginet.net@gmail.com>
2014-04-23 14:03:39 +0900

    Inplement blog views

---
+++ src/kawaz/apps/blogs/urls.py
@@ -0,0 +1,37 @@
+from django.conf.urls import patterns, include, url
+
+from .views import EntryAuthorDayArchiveView
+from .views import EntryAuthorListView
+from .views import EntryAuthorMonthArchiveView
+from .views import EntryAuthorTodayArchiveView
+from .views import EntryAuthorYearArchiveView
+from .views import EntryCreateView
+from .views import EntryDayArchiveView
+from .views import EntryDeleteView
+from .views import EntryDetailView
+from .views import EntryListView
+from .views import EntryMonthArchiveView
+from .views import EntryTodayArchiveView
+from .views import EntryUpdateView
+from .views import EntryYearArchiveView
+
+author_patterns = ('',
+   url(r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/(?P<object_id>\d+)/$', EntryDetailView.as_view(), name='blogs_entry_detail'),
+   url(r'^create/$', EntryCreateView.as_view(), name='blogs_entry_create'),
+   url(r'^(?P<object_id>\d+)/update/$', EntryUpdateView.as_view(), name='blogs_entry_update'),
+   url(r'^(?P<object_id>\d+)/delete/$', EntryDeleteView.as_view(), name='blogs_entry_delete'),
+   url(r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/$', EntryAuthorDayArchiveView.as_view(), name='blogs_entry_author_archive-day'),
+   url(r'^(?P<year>\d+)/(?P<month>\d+)/$', EntryAuthorMonthArchiveView.as_view(), name='blogs_entry_author_archive-month'),
+   url(r'^(?P<year>\d+)/$', EntryAuthorYearArchiveView.as_view(), name='blogs_entry_author_archive-year'),
+   url(r'^today/$', EntryAuthorTodayArchiveView.as_view(), name='blogs_entry_author_archive-today'),
+   url(r'^$', EntryAuthorListView.as_view(), name='blogs_author_entry_list'),
+)
+
+urlpatterns = patterns('',
+    url(r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/$', EntryDayArchiveView.as_view(), name='blogs_entry_archive-day'),
+    url(r'^(?P<year>\d+)/(?P<month>\d+)/$', EntryMonthArchiveView.as_view(), name='blogs_entry_archive-month'),
+    url(r'^(?P<year>\d+)/$', EntryYearArchiveView.as_view(), name='blogs_entry_archive-year'),
+    url(r'^$', EntryListView.as_view(), name='blogs_entry_list'),
+    url(r'^today/$', EntryTodayArchiveView.as_view(), name='blogs_entry_archive-today'),
+    (r'^(?P<author>\w+)/', include(author_patterns)),
+)
--- src/kawaz/apps/blogs/views.py
+++ src/kawaz/apps/blogs/views.py
@@ -1,3 +1,115 @@
-from django.shortcuts import render
+from django.views.generic import UpdateView
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import DetailView
+from django.views.generic import ListView
+from django.views.generic import TodayArchiveView
+from django.views.generic import DayArchiveView
+from django.views.generic import MonthArchiveView
+from django.views.generic import YearArchiveView
+from django.views.generic.dates import MultipleObjectMixin
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+
+from kawaz.core.personas.models import Persona
+
+from .models import Entry
+
+class EntryListView(ListView):
+    '''
+    View class for listing all entries
+    '''
+    model = Entry
+
+
+class EntryDetailView(DetailView):
+    '''
+    View class for details of blog entries.
+    '''
+    model = Entry
+
+
+class EntryCreateView(CreateView):
+    '''
+    View class for blog entry creation
+    '''
+    model = Entry
+
+
+class EntryUpdateView(UpdateView):
+    '''
+    View class for updating blog entries
+    '''
+    model = Entry
+
+
+class EntryDeleteView(DeleteView):
+    '''
+    View class for deleting blog entries.
+    '''
+    model = Entry
+
+
+class EntryTodayArchiveView(TodayArchiveView):
+    '''
+    View class for listing blog entries written on today.
+    '''
+    model = Entry
+
+
+class EntryDayArchiveView(DayArchiveView):
+    '''
+    View class for listing blog entries written in the day.
+    '''
+    model = Entry
+
+
+class EntryMonthArchiveView(MonthArchiveView):
+    '''
+    View class for listing blog entries written in the month
+    '''
+    model = Entry
+
+
+class EntryYearArchiveView(YearArchiveView):
+    '''
+    View class for listing blog entries written in the year.
+    '''
+    model = Entry
+
+
+class EntryAuthorMixin(MultipleObjectMixin):
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        username = self.kwargs.get('author')
+        author = get_object_or_404(Persona, username=username)
+        return qs.filter(author=author)
+
+
+class EntryAuthorListView(EntryListView, EntryAuthorMixin):
+    '''
+    View class for listing for all entries of specific author.
+    '''
+
+class EntryAuthorTodayArchiveView(EntryTodayArchiveView, EntryAuthorMixin):
+    '''
+    View class for listing for entries written on today of specific author.
+    '''
+
+
+class EntryAuthorDayArchiveView(EntryDayArchiveView, EntryAuthorMixin):
+    '''
+    View class for listing for entries written in the day of specific author.
+    '''
+
+
+class EntryAuthorMonthArchiveView(EntryMonthArchiveView, EntryAuthorMixin):
+    '''
+    View class for listing for entries written in the month of specific author.
+    '''
+
+class EntryAuthorYearArchiveView(EntryYearArchiveView, EntryAuthorMixin):
+    '''
+    View class for listing for entries written in the year of specific author.
+    '''
