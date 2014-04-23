36902880e60de661c1e0f155ba3db5d8ce3b44b7
giginet <giginet.net@gmail.com>
2014-04-23 15:20:41 +0900

    Add templates for blog entry views

--- src/kawaz/urls.py
+++ src/kawaz/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^events/', include('kawaz.apps.events.urls')),
     url(r'^members/', include('kawaz.apps.profiles.urls')),
+    url(r'^blogs/', include('kawaz.apps.blogs.urls')),
     url(r'^$', IndexView.as_view(), name='kawaz_index')
 )
