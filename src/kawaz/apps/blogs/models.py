f5a6f17c1616047141209bb5248950d826a764f5
giginet <giginet.net@gmail.com>
2014-04-23 16:56:35 +0900

    Implement EntryManager and add its specs

--- src/kawaz/apps/blogs/models.py
+++ src/kawaz/apps/blogs/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext as _
 from django.core.exceptions import ValidationError
@@ -22,6 +23,24 @@ class Category(models.Model):
     def __str__(self):
         return '%s(%s)' % (self.label, self.author.username)
 
+
+class EntryManager(models.Model):
+    '''The model manager for Entry'''
+
+    def published(self, user):
+        '''Returns Queryset which contains viewable objects by ``user``.'''
+        q = Q(pub_state='public')
+        if user and user.is_authenticated():
+            q |= Q(pub_state='protected')
+        return self.filter(q).distinct()
+
+    def draft(self, user):
+        '''Returns Queryset contains draft entries which owned by ``user``.'''
+        if user and user.is_authenticated():
+            return self.filter(organizer=user, pub_state='draft')
+        return self.none()
+
+
 @validate_on_save
 class Entry(models.Model):
     '''Entry model of blog'''
@@ -36,6 +55,8 @@ class Entry(models.Model):
     updated_at = models.DateTimeField(_('Modified at'), auto_now=True)
     publish_at = models.DateTimeField(_('Published at'), null=True, editable=False)
 
+    objects = EntryManager()
+
     class Meta:
         ordering = ('-updated_at', 'title')
         unique_together = (('title', 'author',),)
@@ -49,10 +70,11 @@ class Entry(models.Model):
         return self.title
 
     def save(self, *args, **kwargs):
-        if self.pk == None:
+        if not self.publish_at:
             if self.pub_state == 'draft':
+                # if drafted and haven't published yet set publish_at = None
                 self.publish_at = None
-            elif self.publish_at == None:
+            else:
                 self.publish_at = datetime.datetime.now()
         super().save(*args, **kwargs)
 
@@ -60,6 +82,11 @@ class Entry(models.Model):
         if self.category and self.author != self.category.author:
             raise ValidationError('Category must be owned by author.')
         super().clean()
+
+    @property
+    def publish_at_date(self):
+        '''return Publish date'''
+        return datetime.datetime.date(self.publish_at)
 
 from permission import add_permission_logic
 from permission.logics.author import AuthorPermissionLogic
