6d1e4d7611112ce2c26fcfbeabac3929a5919e19
giginet <giginet.net@gmail.com>
2014-05-04 04:08:39 +0900

    Add views for Project

--- src/kawaz/apps/projects/views.py
+++ src/kawaz/apps/projects/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import UpdateView
 
-# Create your views here.
+from .models import Project
+
+from permission.decorators import permission_required
+
+@permission_required('projects.add_project')
+class ProjectCreateView(CreateView):
+    model = Project
+
+
+@permission_required('projects.edit_project')
+class ProjectUpdateView(UpdateView):
+    model = Project
+
+
+@permission_required('projects.delete_project')
+class ProjectDeleteView(DeleteView):
+    model = Project
+
+
+@permission_required('projects.view_project')
+class ProjectDetailView(DetailView):
+    model = Project
+
+
+class ProjectListView(ListView):
+    model = Project
+
+
+class ProjectAuthorListView(ListView):
+    model = Project
+
+
+class ProjectCategoryListView(ListView):
+    model = Project
