905723ece4064f7f7363e99160e739c7453582bf
giginet <giginet.net@gmail.com>
2014-05-14 12:31:50 +0900

    Add django-tastypie

---
+++ src/kawaz/core/api/urls.py
@@ -0,0 +1,4 @@
+from django.conf.urls.defaults import *
+from tastypie.api import Api
+
+v1_api = Api(api_name='v1')
--- src/kawaz/urls.py
+++ src/kawaz/urls.py
@@ -2,11 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from kawaz.core.views import IndexView
+from kawaz.core.api.urls import v1_api
+
+from django.conf.urls.defaults import *
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(v1_api.urls)),
     url(r'^announcements/', include('kawaz.apps.announcements.urls')),
     url(r'^events/', include('kawaz.apps.events.urls')),
     url(r'^members/', include('kawaz.apps.profiles.urls')),
