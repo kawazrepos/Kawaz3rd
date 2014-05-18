9010b10e1b16ebec5f0413839291f22f52a123ed
giginet <giginet.net@gmail.com>
2014-05-18 21:21:20 +0900

    Add Media URL. Fix Static URL

--- src/kawaz/urls.py
+++ src/kawaz/urls.py
@@ -7,7 +7,9 @@ from kawaz.core.views import IndexView
 from kawaz.core.api import v1_api
 
 admin.autodiscover()
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from django.conf import settings
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^announcements/', include('kawaz.apps.announcements.urls')),
@@ -18,4 +20,5 @@ urlpatterns = patterns('',
     url(r'^stars/', include('kawaz.apps.stars.urls')),
     url(r'^$', IndexView.as_view(), name='kawaz_index'),
     url(r'^api/', include(v1_api.urls)),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
