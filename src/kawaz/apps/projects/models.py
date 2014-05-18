c4e34512e2cc9bc862c2aabd2254820303f018b8
Λlisue <lambdalisue@hashnote.net>
2014-05-18 13:22:50 +0900

    Follow PEP8

--- src/kawaz/apps/projects/models.py
+++ src/kawaz/apps/projects/models.py
@@ -6,21 +6,19 @@ from django.core.exceptions import PermissionDenied
 from django.utils.translation import ugettext as _
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
-
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 from thumbnailfield.fields import ThumbnailField
 from markupfield.fields import MarkupField
 
 from kawaz.core.permissions.logics import PUB_STATES
+
 
 class Category(models.Model):
     """
     This model indicates category of each projects
     """
     label = models.CharField(_('Name'), max_length=32, unique=True)
-    parent = models.ForeignKey('self', verbose_name=_('Parent category'), null=True, blank=True, related_name='children')
+    parent = models.ForeignKey('self', verbose_name=_('Parent category'),
+                               null=True, blank=True, related_name='children')
 
     class Meta:
         ordering = ('label',)
@@ -65,19 +63,39 @@ class Project(models.Model):
     )
 
     # Required
-    pub_state = models.CharField(_('Publish status'), choices=PUB_STATES, max_length=10, default='public')
-    status = models.CharField(_("Status"), default="planning", max_length=15, choices=STATUS)
+    pub_state = models.CharField(_('Publish status'), choices=PUB_STATES,
+                                 max_length=10, default='public')
+    status = models.CharField(_("Status"), default="planning",
+                              max_length=15, choices=STATUS)
     title = models.CharField(_('Title'), max_length=127, unique=True)
     slug = models.SlugField(_('Project ID'), unique=True, max_length=63,
-                            help_text=_("This ID will be used for its URL. You can't modify it later. You can use only alphabetical characters, _ or -."))
+                            help_text=_("This ID will be used for its URL. "
+                                        "You can't modify it later. "
+                                        "You can use only alphabetical "
+                                        "characters or some delimiters ('_' or "
+                                        "'-')."))
     body = MarkupField(_('Description'), default_markup_type='markdown')
     # Omittable
-    icon = ThumbnailField(_('Thumbnail'), upload_to=_get_upload_path, blank=True, patterns=settings.THUMBNAIL_SIZE_PATTERNS)
-    category = models.ForeignKey(Category, verbose_name=_('Category'), null=True, blank=True, related_name='projects', help_text="If a category you would like to use is not exist, please contact your administrator.")
+    icon = ThumbnailField(_('Thumbnail'), upload_to=_get_upload_path,
+                          blank=True, 
+                          patterns=settings.THUMBNAIL_SIZE_PATTERNS)
+    category = models.ForeignKey(Category, verbose_name=_('Category'),
+                                 null=True, blank=True,
+                                 related_name='projects',
+                                 help_text=_("If a category you would like to "
+                                             "use is not exist, please contact "
+                                             "your administrator."))
     # Uneditable
-    administrator = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('Administrator'), related_name="projects_owned", editable=False)
-    members = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_('Members'), related_name="projects_joined", editable=False)
-    group = models.ForeignKey(Group, verbose_name=_('Group'), unique=True, editable=False)
+    administrator = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                      verbose_name=_('Administrator'),
+                                      related_name="projects_owned",
+                                      editable=False)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                     verbose_name=_('Members'),
+                                     related_name="projects_joined",
+                                     editable=False)
+    group = models.ForeignKey(Group, verbose_name=_('Group'),
+                              unique=True, editable=False)
     created_at = models.DateTimeField(_('Created at'), auto_now_add=True)
     updated_at = models.DateTimeField(_('Updated at'), auto_now=True)
 
@@ -98,8 +116,8 @@ class Project(models.Model):
 
     def save(self, *args, **kwargs):
         if self.pk is None:
-            group = Group.objects.get_or_create(name="project_%s" % self.slug)[0]
-            self.group = group
+            group_name = "project_" + self.slug
+            self.group = Group.objects.get_or_create(name=group_name)[0]
         return super().save(*args, **kwargs)
 
     def join(self, user, save=True):
@@ -134,12 +152,17 @@ class Project(models.Model):
             'slug' : self.slug
         })
 
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 @receiver(post_save, sender=Project)
 def join_administrator(**kwargs):
     created = kwargs.get('created')
     instance = kwargs.get('instance')
     if created and instance.pub_state != 'draft':
         instance.join(instance.administrator)
+
 
 from permission import add_permission_logic
 from permission.logics import AuthorPermissionLogic
