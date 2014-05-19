3e04e952dfd49a534ab9a5bf54bfdf6a8e14f79a
giginet <giginet.net@gmail.com>
2014-05-20 06:13:47 +0900

    Add contexts to AnonymousIndex

--- src/kawaz/core/views/tests/test_views.py
+++ src/kawaz/core/views/tests/test_views.py
@@ -6,18 +6,39 @@ from kawaz.core.personas.tests.factories import PersonaFactory
 class KawazIndexViewTestCase(TestCase):
 
     def test_anonymous_user(self):
-        '''Tests an anonymous user can view anonymous index'''
-        url = reverse('kawaz_index')
-        response = self.client.get(url)
-        self.assertTemplateUsed(response.template_name, 'core/anonymous_index.html')
+        """
+        非ログインユーザーはAnonymousIndexにリダイレクトされます
+        """
+        response = self.client.get("")
+        self.assertTemplateUsed(response, 'core/anonymous_index.html')
 
     def test_authorized_user(self):
-        '''Tests an authorized user can view anonymous index'''
+        """
+        ログインユーザーはAuthorizedIndexにリダイレクトされます
+        """
         user = PersonaFactory()
-        user.set_password('password')
-        user.save()
 
-        url = reverse('kawaz_index')
         self.assertTrue(self.client.login(username=user.username, password='password'))
-        response = self.client.get(url)
-        self.assertTemplateUsed(response.template_name, 'core/anonymous_index.html')
+        response = self.client.get("")
+        self.assertTemplateUsed(response, 'core/authenticated_index.html')
+
+
+class KawazAnonymousIndexViewTestCase(TestCase):
+
+    def test_unauthorized_context(self):
+        """
+        非ログインユーザーがトップにアクセスしたとき、以下のQuerySetがcontextに含まれます
+        recent_products : display_mode = normalのプロダクトリリース日順3件
+        featured_products : display_mode = featureのリリース日順全て
+        products : display_mode = feature || tiled のリリース日順全て
+        entries : publicなブログ記事公開日順5件
+        events : publicでアクティブなイベント開催日時が近い順5件
+        announcements : publicなお知らせ公開日順5件
+        """
+        r = self.client.get("/")
+        self.assertIsNotNone(r.context['recent_products'])
+        self.assertIsNotNone(r.context['featured_products'])
+        self.assertIsNotNone(r.context['products'])
+        self.assertIsNotNone(r.context['entries'])
+        self.assertIsNotNone(r.context['events'])
+        self.assertIsNotNone(r.context['announcements'])
