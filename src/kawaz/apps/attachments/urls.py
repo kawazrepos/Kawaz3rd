17b3af4e8d22ba30b9eb06252f3e317a7af770ce
giginet <giginet.net@gmail.com>
2014-05-24 03:52:04 +0900

    Implement MaterialDetailView

--- src/kawaz/apps/attachments/urls.py
+++ src/kawaz/apps/attachments/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import patterns, url
 
 from kawaz.core.api import v1_api
 from .api.resources import MaterialResource
+from .views import MaterialDetailView
 
 v1_api.register(MaterialResource())
 
 urlpatterns = patterns('',
+    url(r'^(?P<slug>[^/]+)/$', MaterialDetailView.as_view(), name='materials_material_detail'),
 )
--- src/kawaz/apps/attachments/views.py
+++ src/kawaz/apps/attachments/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import os
+import mimetypes
+from django.conf import settings
+from django.views.generic.detail import BaseDetailView
+from django.http.response import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+from .models import Material
+
+class MaterialDetailView(BaseDetailView):
+    model = Material
+    slug_field = 'slug'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        try:
+            name = self.object.content_file.name
+            mime_type_guess = mimetypes.guess_type(name)
+            path = os.path.join(settings.MEDIA_ROOT, object.content_file.path)
+            with open(path, 'r') as file:
+                response = HttpResponse(file, mimetype=mime_type_guess[0])
+                response['Content-Disposition'] = 'attachment; filename={}'.format(name)
+        except:
+            pass
+        return HttpResponseNotFound()
