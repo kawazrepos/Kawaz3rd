74ad3432230d87ba9cbb49d1eeefbf764d2a1577
Λlisue <lambdalisue@hashnote.net>
2014-05-28 13:40:06 +0900

    Rename repository name to Kawaz3rd

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # coding=utf-8
-import sys
 from setuptools import setup, find_packages
 
-NAME = 'third-impact'
+NAME = 'Kawaz3rd'
 VERSION = '0.1.0'
+
 
 def read(filename):
     import os
@@ -12,17 +12,19 @@ def read(filename):
     with open(filename, 'r') as fi:
         return fi.read()
 
+
 def readlist(filename):
     rows = read(filename).split("\n")
     rows = [x.strip() for x in rows if x.strip()]
     return list(rows)
 
+
 setup(
-    name = NAME,
-    version = VERSION,
-    description = "A portal website of Sapporo Game Creators' Community Kawaz",
-    long_description = read('README.rst'),
-    classifiers = (
+    name=NAME,
+    version=VERSION,
+    description="A portal website of Sapporo Game Creators' Community Kawaz",
+    long_description=read('README.rst'),
+    classifiers=(
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -34,16 +36,16 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP',
     ),
-    keywords = "django Kawaz",
-    author = 'Kawaz',
-    author_email = 'webmaster@kawaz.org',
-    url = 'https://github.com/kawazrepos/%s' % NAME,
-    download_url = 'https://github.com/kawazrepos/%s/tarball/master' % NAME,
-    license = 'MIT',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    include_package_data = True,
-    package_data = {
+    keywords="django Kawaz",
+    author='Kawaz',
+    author_email='webmaster@kawaz.org',
+    url='https://github.com/kawazrepos/%s' % NAME,
+    download_url='https://github.com/kawazrepos/%s/tarball/master' % NAME,
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    package_data={
         '': ['README.rst',
              'requirements.txt',
              'requirements-test.txt',
@@ -52,5 +54,8 @@ setup(
     zip_safe=True,
     install_requires=readlist('requirements.txt'),
     test_suite="runtests.runtests",
-    tests_require=readlist('requirements.txt')+readlist('requirements-test.txt'),
+    tests_require=(
+        readlist('requirements.txt') +
+        readlist('requirements-test.txt')
+    )
 )
