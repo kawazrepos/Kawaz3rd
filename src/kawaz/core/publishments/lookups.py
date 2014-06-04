99503e64a6c1ac3e2c929bb09ed46f923473c772
Λlisue <lambdalisue@hashnote.net>
2014-06-04 23:43:02 +0900

    コメントの日本語化

--- src/kawaz/core/publishments/lookups.py
+++ src/kawaz/core/publishments/lookups.py
@@ -1,25 +1,21 @@
-# coding=utf-8
-"""
-"""
-__author__ = 'Alisue <lambdalisue@hashnote.net>'
 from django.db.models import Q
 
 
 def published_lookup(user_obj, field_name='pub_state'):
     """
-    Return an instance of Q object to lookup the objects which is published to
-    the specified user.
+    指定されたユーザーが閲覧可能な公開オブジェクトをフィルタするための
+    Qオブジェクトを返す
 
-    If the user is Kawaz member, the lookup return the public and protected
-    objects otherwise only public objects.
+    ユーザーがメンバーであれば、publicおよびprotected指定のオブジェクトを返し
+    それ以外であればpublic指定のオブジェクトを返す
 
     Args:
-        user_obj (obj): User model instance (or AnonymousUser instance)
-        field_name (str): A name of the field which represent the publishment
-            status (Default: ``'pub_state'``)
+        user_obj (obj): Userモデルインスタンス（or AnonymousUser）
+        field_name (str): 公開状態を指定しているフィールド名
+            （デフォルト： ``'pub_state'``）
 
     Returns:
-        An instance of Q object
+        Qオブジェクトインスタンス
     """
     q = Q(**{field_name: 'public'})
     if user_obj and user_obj.is_authenticated() and user_obj.is_member:
@@ -29,26 +25,29 @@ def published_lookup(user_obj, field_name='pub_state'):
 
 def draft_lookup(user_obj, author_field_name='author', field_name='pub_state'):
     """
-    Return an instance of Q object to lookup the draft objects of the specified
-    user.
+    指定されたユーザーが編集可能な下書きオブジェクトをフィルタするための
+    Qオブジェクトを返す
 
-    If the user is Kawaz member, the lookup return own draft objects otherwise
-    non objects.
+    ユーザーがメンバーであれば、自身が保有する下書き状態のオブジェクトを返し
+    それ以外であれば何も返さない。なおユーザーがsuperuserの場合は存在する
+    あらゆる下書き状態のオブジェクトを返す（superuserはあらゆる権限を持つため
+    全ての下書き記事の編集権限を持つ）
 
     Args:
-        user_obj (obj): User model instance (or AnonymousUser instance)
-        author_field_name (str): A name of the foreign field which represent
-            the author user (Default: ``'author'``)
-        field_name (str): A name of the field which represent the publishment
-            status (Default: ``'pub_state'``)
-
+        user_obj (obj): Userモデルインスタンス（or AnonymousUser）
+        author_field_name (str): 所有者を指定しているフィールド名
+            （デフォルト: ``'author'``）
+        field_name (str): 公開状態を指定しているフィールド名
+            （デフォルト： ``'pub_state'``）
     Returns:
-        An instance of Q object
+        Qオブジェクトインスタンス
     """
-    # pk = -1 never exists
+    # pk = -1 は db 的に存在しない値なので何も返さないフィルタになる
     q = Q(pk=-1)
     if user_obj and user_obj.is_authenticated():
         if user_obj.is_superuser:
+            # superuser は全てのオブジェクトに対し編集権限を持つため所有者
+            # チェックは行わない
             q = Q(**{field_name: 'draft'})
         elif user_obj.is_member:
             q = Q(**{author_field_name: user_obj, field_name: 'draft'})
