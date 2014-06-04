377ee935c61dfade0bd073c5536d0af77f6704c3
Λlisue <lambdalisue@hashnote.net>
2014-06-05 05:30:22 +0900

    Publisher導入に向けてViewによるコンテキストの配信を廃止

--- src/kawaz/core/views/__init__.py
+++ src/kawaz/core/views/__init__.py
@@ -1,34 +1,16 @@
 import os
 from django.views.generic.base import TemplateView
-from django.db.models import Q
-from django.contrib.auth.models import AnonymousUser
 
-from kawaz.apps.products.models import Product
-from kawaz.apps.events.models import Event
-from kawaz.apps.blogs.models import Entry
-from kawaz.apps.announcements.models import Announcement
 
 class AnonymousIndexView(TemplateView):
     base_dir = 'core'
     template_name = os.path.join(base_dir, 'anonymous_index.html')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        featured = Q(display_mode='featured')
-        tiled = Q(display_mode='tiled')
-        # ログインユーザーが非ログイントップを見ても必ず公開記事のみを返します
-        user = AnonymousUser()
-        context['recent_products'] = Product.objects.filter(display_mode='normal').order_by('-publish_at')[:3]
-        context['featured_products'] = Product.objects.filter(featured).order_by('-publish_at')
-        context['products'] = Product.objects.filter(featured | tiled).order_by('-publish_at')
-        context['entries'] = Entry.objects.published(user).order_by('-publish_at')[:5]
-        context['events'] = Event.objects.active(user)[:5]
-        context['announcements'] = Announcement.objects.published(user).order_by('-created_at')[:5]
-        return context
 
 class AuthenticatedIndexView(TemplateView):
     base_dir = 'core'
     template_name = os.path.join(base_dir, 'authenticated_index.html')
+
 
 def get_index_view(request, *args, **kwargs):
     """
--- src/kawaz/core/views/tests/test_views.py
+++ src/kawaz/core/views/tests/test_views.py
@@ -1,10 +1,9 @@
 from django.test import TestCase
-from django.core.urlresolvers import reverse
 
 from kawaz.core.personas.tests.factories import PersonaFactory
 
-class KawazIndexViewTestCase(TestCase):
 
+class KawazIndexViewTestCase(TestCase):
     def test_anonymous_user(self):
         """
         非ログインユーザーはAnonymousIndexにリダイレクトされます
@@ -18,27 +17,7 @@ class KawazIndexViewTestCase(TestCase):
         """
         user = PersonaFactory()
 
-        self.assertTrue(self.client.login(username=user.username, password='password'))
+        self.assertTrue(self.client.login(username=user.username,
+                                          password='password'))
         response = self.client.get("")
         self.assertTemplateUsed(response, 'core/authenticated_index.html')
-
-
-class KawazAnonymousIndexViewTestCase(TestCase):
-
-    def test_unauthorized_context(self):
-        """
-        非ログインユーザーがトップにアクセスしたとき、以下のQuerySetがcontextに含まれます
-        recent_products : display_mode = normalのプロダクトリリース日順3件
-        featured_products : display_mode = featureのリリース日順全て
-        products : display_mode = feature || tiled のリリース日順全て
-        entries : publicなブログ記事公開日順5件
-        events : publicでアクティブなイベント開催日時が近い順5件
-        announcements : publicなお知らせ公開日順5件
-        """
-        r = self.client.get("/")
-        self.assertIsNotNone(r.context['recent_products'])
-        self.assertIsNotNone(r.context['featured_products'])
-        self.assertIsNotNone(r.context['products'])
-        self.assertIsNotNone(r.context['entries'])
-        self.assertIsNotNone(r.context['events'])
-        self.assertIsNotNone(r.context['announcements'])
