702dc0b527c32a3fe5a775b5dcd1f43017a038a8
Λlisue <lambdalisue@hashnote.net>
2014-06-05 04:27:30 +0900

    パーミッションテスト用に新しいヘルパー関数を追加

--- src/kawaz/core/utils/permission.py
+++ src/kawaz/core/utils/permission.py
@@ -2,6 +2,32 @@
 """
 """
 __author__ = 'Alisue <lambdalisue@hashnote.net>'
+from django.core.exceptions import ObjectDoesNotExist
+from permission.utils.permissions import perm_to_permission
+
+
+def check_object_permission(user_obj, codename, obj):
+    """
+    指定ユーザが省略形パーミッションを指定オブジェクトに対して持つか調べる
+
+    Args:
+        user_obj (user instance): 対象ユーザインスタンス
+        codename (str): 省略形パーミッション（例: `'add'`）
+        obj (model instance): 対象モデルインスタンス
+
+    Returns:
+        bool or None: 指定されたパーミッションが存在する場合は`True`/`False`を
+            返し、存在しない場合は`None`を返す
+    """
+    try:
+        perm = get_full_permission_name(codename, obj)
+        # 文字列 permission を実体に変換（してみる）
+        perm_to_permission(perm)
+        # 指定されたパーミッションが存在するためチェックを行う
+        return user_obj.has_perm(perm, obj=obj)
+    except ObjectDoesNotExist:
+        # 指定されたパーミッションが存在しないため None を返す
+        return None
 
 
 def get_full_permission_name(codename, obj):
