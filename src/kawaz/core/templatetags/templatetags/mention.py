ceb13ee91c86b793f97e368108c66f67d3401dc0
giginet <giginet.net@gmail.com>
2014-06-13 06:49:36 +0900

    mentionテンプレートフィルタのテストを追加

--- src/kawaz/core/templatetags/templatetags/mention.py
+++ src/kawaz/core/templatetags/templatetags/mention.py
@@ -1,11 +1,12 @@
 import re
 from django import template
+from django.core.exceptions import ObjectDoesNotExist
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 
 from kawaz.core.personas.models import Persona
 
-USERNAME_PATTERN = r'@(P<username>[0-9a-zA-Z-_]+)'
+USERNAME_PATTERN = r'@(?P<username>[0-9a-zA-Z-_]+)'
 
 register = template.Library()
 
@@ -18,12 +19,13 @@ def mention(value):
     regex = re.compile(USERNAME_PATTERN)
     def repl(m):
         username = m.group('username')
-        user = Persona.objects.get(username=username)
-        if user:
+        try:
+            user = Persona.objects.get(username=username)
             html = render_to_string("templatetags/mention.html", {
                 'user' : user
             })
             return html
-        return "@{}".format(username)
+        except ObjectDoesNotExist:
+            return "@{}".format(username)
     value = regex.sub(repl, value)
     return mark_safe(value)
---
+++ src/kawaz/core/templatetags/tests/test_mention.py
@@ -0,0 +1,91 @@
+#! -*- coding: utf-8 -*-
+#
+# created by giginet on 2014/6/9
+#
+__author__ = 'giginet'
+
+from django.test import TestCase
+from django.template import Template, Context
+from django.template.loader import render_to_string
+from kawaz.core.personas.tests.factories import PersonaFactory
+
+class MentionTemplateTagTestCase(TestCase):
+
+    def _test_mention_tag(self, before, after):
+        c = Context({
+            'body' : before
+        })
+        t = Template(
+            """{% load mention %}"""
+            """{{ body | mention }}"""
+        )
+        r = t.render(c)
+        self.assertEqual(r.strip(), after.strip())
+
+    def _expand_mention_tag(self, user):
+        html = render_to_string("templatetags/mention.html", {
+            'user' : user
+        })
+        return html
+
+    def test_with_exist_user(self):
+        """
+        存在するユーザーに対して言及したとき、展開される
+        """
+        kawaztan = PersonaFactory(username='kawaztan')
+        tag = self._expand_mention_tag(kawaztan)
+        before = """
+        みんなのアイドルです @kawaztan
+        """
+        after = """
+        みんなのアイドルです {}
+        """.format(tag)
+
+        self._test_mention_tag(before, after)
+
+    def test_with_unknown_user(self):
+        """
+        存在しないユーザーに対して言及したとき、展開されない
+        """
+        before = """
+        みんなのアイドルです @unknown_kawaztan
+        """
+        after = before
+
+        self._test_mention_tag(before, after)
+
+    def test_with_exist_user_multiple(self):
+        """
+        複数の存在するユーザーに対して言及したとき、展開される
+        """
+        kawaztan = PersonaFactory(username='kawaztan')
+        geekdrums = PersonaFactory(username='geekdrums')
+        tag = self._expand_mention_tag(kawaztan)
+        tag2 = self._expand_mention_tag(geekdrums)
+        before = """
+        みんなのアイドルです @kawaztan
+        神、いわゆるゴッドです @geekdrums
+        """
+        after = """
+        みんなのアイドルです {}
+        神、いわゆるゴッドです {}
+        """.format(tag, tag2)
+
+        self._test_mention_tag(before, after)
+
+    def test_with_unknown_user_multiple(self):
+        """
+        複数の存在するユーザーと存在しないユーザーを混ぜて言及したとき、展開される
+        """
+        geekdrums = PersonaFactory(username='geekdrums')
+        tag = self._expand_mention_tag(geekdrums)
+        before = """
+        みんなのアイドルです @unknown_kawaztan
+        神、いわゆるゴッドです @geekdrums
+        """
+        after = """
+        みんなのアイドルです @unknown_kawaztan
+        神、いわゆるゴッドです {}
+        """.format(tag)
+
+        self._test_mention_tag(before, after)
