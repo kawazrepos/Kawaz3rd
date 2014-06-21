6e161bb8d118e3cccae510b122c469a925dc545b
giginet <giginet.net@gmail.com>
2014-06-21 23:46:57 +0900

    Parser内で使われているtemplate filterの詳細を追加

--- src/kawaz/core/templatetags/templatetags/parser.py
+++ src/kawaz/core/templatetags/templatetags/parser.py
@@ -14,9 +14,17 @@ def parser(value):
     Kawazで使用するtemplate tagsをまとめて使用するtemplate filterです
     以下の処理を行います
         YouTube展開
+            YouTubeの動画URLをプレーヤーとして展開します
+            ref: kawaz.core.templatetags.tempaltetags.embed.youtube
         ニコニコ動画展開
+            ニコニコ動画の動画URLをプレーヤーとして展開します
+            ref: kawaz.core.templatetags.tempaltetags.embed.nicovideo
         URLをリンクに展開
+            URLやメールアドレスの文字列をリンクタグに変換します
+            ref: django.template.defaultfilters.urlize
         @usernameを展開
+            @username という文字列をユーザーへのリンクタグに変換します
+            ref: kawaz.core.templatetags.tempaltetags.mention.mention
     """
     # URL
     # Youtube, NicoNico
