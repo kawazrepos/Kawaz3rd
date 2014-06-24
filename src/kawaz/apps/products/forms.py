82815ef177545dac13b1ce81b5b465e675496796
giginet <giginet.net@gmail.com>
2014-06-24 23:49:35 +0900

    ProfileフォームにAccount Formsetを追加した [CI Skip]

--- src/kawaz/apps/products/forms.py
+++ src/kawaz/apps/products/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.forms import widgets
 from django.forms import ModelForm
+from django.forms.models import inlineformset_factory, BaseInlineFormSet
 from .models import Product
 from .models import Platform
 from .models import Category
+from .models import Screenshot
 
 class ProductBaseForm(ModelForm):
     platforms = forms.ModelMultipleChoiceField(widget=widgets.CheckboxSelectMultiple,
@@ -24,3 +26,13 @@ class ProductUpdateForm(ProductBaseForm):
                 'description_markup_type', 'slug', 
                 'administrators', 'display_mode'
             )
+
+class ScreenshotForm(ModelForm):
+    class Meta:
+        model = Screenshot
+        fields = ('image',)
+
+ScreenshotFormSet = inlineformset_factory(Product, Screenshot, extra=1, can_delete=True)
+
+def get_screenshot_formset(form, formset=BaseInlineFormSet, *args, **kwargs):
+    return inlineformset_factory(Product, Screenshot, form, formset, **kwargs)
