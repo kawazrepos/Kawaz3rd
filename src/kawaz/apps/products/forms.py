58f7d81e7d17885580d9f8c0903f18f5b435995b
giginet <giginet.net@gmail.com>
2014-06-24 21:54:11 +0900

    ProductフォームのWidgetを変更

--- src/kawaz/apps/products/forms.py
+++ src/kawaz/apps/products/forms.py
@@ -1,14 +1,25 @@
+from django import forms
+from django.forms import widgets
 from django.forms import ModelForm
 from .models import Product
+from .models import Platform
+from .models import Category
 
-class ProductCreateForm(ModelForm):
+class ProductBaseForm(ModelForm):
+    platforms = forms.ModelMultipleChoiceField(widget=widgets.CheckboxSelectMultiple,
+                                               queryset=Platform.objects.all().order_by('pk'))
+    categories = forms.ModelMultipleChoiceField(widget=widgets.CheckboxSelectMultiple,
+                                                queryset=Category.objects.all().order_by('pk'))
+
     class Meta:
         model = Product
+
+class ProductCreateForm(ProductBaseForm):
+    class Meta(ProductBaseForm.Meta):
         exclude = ('description_markup_type', 'administrators', 'display_mode')
 
-class ProductUpdateForm(ModelForm):
-    class Meta:
-        model = Product
+class ProductUpdateForm(ProductBaseForm):
+    class Meta(ProductBaseForm.Meta):
         exclude = (
                 'description_markup_type', 'slug', 
                 'administrators', 'display_mode'
