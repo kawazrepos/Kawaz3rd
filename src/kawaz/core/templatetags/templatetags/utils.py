69b8cb8a33ca1db8b4306579957f66e3f8ed8c32
giginet <giginet.net@gmail.com>
2014-06-29 22:35:22 +0900

    active tagのdocstring書き直した

--- src/kawaz/core/templatetags/templatetags/utils.py
+++ src/kawaz/core/templatetags/templatetags/utils.py
@@ -5,17 +5,15 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def active(context, pattern):
     """
-    現在のURLのregexを渡して、一致していた場合はstringを返します
+    現在のURLのregexを渡して、一致していた場合は'active'を返します
     そうでない場合は空白文字を返します
-    param request Requestオブジェクト
     param pattern 現在のURLの正規表現パターン
-    param string パターンがマッチしたときに返却する文字列
 
     Example:
         {% load utils %}
-        <div class="{% active '/members/.+' 'active' %}"</div>
+        <div class="{% active '/members/.+' %}"</div>
 
-        例えば、このように記述しておくと、bootstrapのタブなど、「特定のページにいるときだけactiveクラスを不可したい」
+        例えば、このように記述しておくと、bootstrapのタブなど、「特定のページにいるときだけactiveクラスを付加したい」
         という需要に対応できます
 
     """
