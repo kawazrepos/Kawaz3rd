ee5692ef56be32bde7e35179be33ec66e1dbc8b8
giginet <giginet.net@gmail.com>
2014-06-30 20:51:27 +0900

    ContextにRequest追加した

--- src/kawaz/settings.py
+++ src/kawaz/settings.py
@@ -135,6 +135,16 @@ TEMPLATE_DIRS = (
     os.path.join(REPOSITORY_ROOT, 'src', 'kawaz', 'templates'),
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages"
+)
+
 LOGIN_URL = '/registration/login/'
 LOGOUT_URL = '/registration/logout/'
 LOGIN_REDIRECT_URL = '/'
