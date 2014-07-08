d0bcca2ee6916009f2d7272ddf69556f080f4a00
giginet <giginet.net@gmail.com>
2014-07-08 09:02:01 +0900

    markdownテンプレートフィルタのテストを書いた

--- src/kawaz/core/templatetags/templatetags/markdown.py
+++ src/kawaz/core/templatetags/templatetags/markdown.py
@@ -12,8 +12,24 @@ register = template.Library()
 @register.filter
 @template.defaultfilters.stringfilter
 def markdown(value):
+    """
+    テキストをmarkdownに展開するtemplate filterです。
+    processorとしてpython-markdown2を使っており、デフォルトの挙動の他に以下のExtrasを含めています
+
+    Extras:
+        footnotes
+            https://github.com/trentm/python-markdown2/wiki/footnotes
+            geekdrums [^foot-note]
+            [^foot-note]: 神、いわゆるゴッド
+        code-friendly
+            https://github.com/trentm/python-markdown2/wiki/code-friendly
+            _hello_, __yo__ などと言った記法を無効化します
+
+    Usage:
+        {% load markdown %}
+        {{ object.body | markdown }}
+    """
     md = markdown2.markdown(value, extras=['footnotes',
-                                           'code-friendly',
-                                           'markdown-in-html'],
+                                           'code-friendly'],
     )
     return mark_safe(md)
