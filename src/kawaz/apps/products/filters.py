782057cfc317c60c8933f15ea00699291c9cb442
Λlisue <lambdalisue@hashnote.net>
2014-07-14 04:51:00 +0900

    django-filters の使い方が間違っていたので修正した
    django-filtersで追加要素を指定したい場合はWidgetレベルでの追加が適しているが
    既存コードは不要なHackを行なっていたために問題が発生していた。
    その部分を本来あるべき姿に直した。
    
    ついでに例としてProductページの絞り込み機能もdjango-filtersを使うように修正を
    行った。
    
    まぁ今回の修正はドキュメントにしっかりと書いてないレベルだから仕方ない。
    django-filtersのソースコードはある程度読み込むことをおすすめする（ただしあまり
    美しくはない）。
    
    Closed #121

--- src/kawaz/apps/products/filters.py
+++ src/kawaz/apps/products/filters.py
@@ -1,26 +1,21 @@
 from django.utils.translation import ugettext as _
-
 import django_filters
 from django_filters import filters
 from django_filters import widgets
-
 from .models import Product
 from .models import Category, Platform
 
-__author__ = 'giginet'
 
 class ProductFilter(django_filters.FilterSet):
+    platforms = filters.ModelChoiceFilter(
+        label=_('Platforms'),
+        queryset=Platform.objects.all(),
+        widget=widgets.LinkWidget(choices=[('', _('All'))]))
 
-    def _choices_with_deselect(qs):
-        """
-        ModelChoiceFilterを利用すると、全ての指定ができなくなってしまう
-        そのため、このようにchoicesをquerysetから生成してChoiceFilterに渡している
-        """
-        choices = [(query.pk, str(query)) for query in qs]
-        return [('', _('Any')),] + choices
-
-    platforms = filters.ChoiceFilter(choices=_choices_with_deselect(Platform.objects.all()), widget=widgets.LinkWidget())
-    categories = filters.ChoiceFilter(choices=_choices_with_deselect(Category.objects.all()), widget=widgets.LinkWidget())
+    categories = filters.ModelChoiceFilter(
+        label=_('Categories'),
+        queryset=Category.objects.all(),
+        widget=widgets.LinkWidget(choices=[('', _('All'))]))
 
     class Meta:
         model = Product
