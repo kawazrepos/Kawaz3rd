95bfc939847d26ee87dcf1aed9c73c76fd85133a
giginet <giginet.net@gmail.com>
2014-07-14 21:32:52 +0900

    真・YoutubeテンプレートタグをResponsive対応した

--- src/kawaz/core/templatetags/templatetags/embed.py
+++ src/kawaz/core/templatetags/templatetags/embed.py
@@ -11,7 +11,7 @@ from django.utils.safestring import mark_safe
 YOUTUBE_PATTERN = re.compile(r"^https?:\/\/www.youtube.com\/watch\?v=(?P<id>[a-zA-Z0-9_-]+)$", flags=re.MULTILINE)
 NICONICO_PATTERN = re.compile(r"^http:/\/\www.nicovideo.jp\/watch\/(?P<id>[a-z]{2}[0-9]+)\/?$", flags=re.MULTILINE)
 DEFAULT_WIDTH = 640
-DEFAULT_HEIGHT = 480
+DEFAULT_HEIGHT = 360
 ASPECT_RATIO = 9.0 / 16.0
 
 register = template.Library()
@@ -24,15 +24,32 @@ def youtube(value, size=None):
     ただし、URL以外の文字列を同じ行に含んではいけません
     これは、プレイヤーの埋め込みという性質上の仕様です
 
+    Size:
+        描画サイズを変更します
+
+        responsive:
+            bootstrapのレスポンシブデザインに対応させ、大きさを指定しません
+            http://getbootstrap.com/components/#responsive-embed
+        width:
+            横幅のみを指定し、16:9のアス比になるように高さを自動指定します
+        width,height:
+            縦幅と横幅を強制的に指定します
+
     Example:
         {{ url | youtube }}
+        {{ url | youtube:'responsive' }}
         {{ url | youtube:'1600' }}
         {{ url | youtube:'1600,900' }}
 
     """
+    is_responsive = False
     if not size:
         width = None
         height =  None
+    elif size == 'responsive':
+        width = None
+        height =  None
+        is_responsive = True
     else:
         bits = size.split(',')
         if len(bits) == 1:
@@ -48,11 +65,18 @@ def youtube(value, size=None):
 
     def repl(m):
         id = m.group('id')
-        html = render_to_string("templatetags/youtube.html", {
-            'video_id' : id,
-            'width': width,
-            'height': height
-        }).replace('\n', '')
+        params = {'video_id': id}
+        if is_responsive:
+            # responsiveのとき、別のテンプレートを使う
+            template = "templatetags/youtube_responsive.html"
+        else:
+            # 大きさが指定されてるとき
+            template = "templatetags/youtube.html"
+            params.update({
+                'width': width,
+                'height': height
+            })
+        html = render_to_string(template, params).replace('\n', '')
         return html
     value = YOUTUBE_PATTERN.sub(repl, value)
     return mark_safe(value)
