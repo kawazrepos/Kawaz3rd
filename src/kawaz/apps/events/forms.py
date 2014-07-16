4f525843c71f6b45b395b35458a1f53af0795460
giginet <giginet.net@gmail.com>
2014-07-17 04:12:35 +0900

    datetime Widgetを追加

--- src/kawaz/apps/events/forms.py
+++ src/kawaz/apps/events/forms.py
@@ -7,6 +7,9 @@ from .models import Event
 class EventForm(ModelForm):
 
     body = forms.CharField(widget=MaceEditorWidget)
+    period_start = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime'}))
+    period_end = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime'}))
+    attendance_deadline = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime'}))
 
     class Meta:
         model = Event
