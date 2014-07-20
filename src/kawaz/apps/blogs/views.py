27e68e9e7e2b77dc9e947a390ed0f354f6330286
giginet <giginet.net@gmail.com>
2014-07-20 18:12:58 +0900

    get_formでブログ作者を設定するようにした

--- src/kawaz/apps/blogs/views.py
+++ src/kawaz/apps/blogs/views.py
@@ -40,16 +40,15 @@ class EntryCreateView(CreateView):
     model = Entry
     form_class = EntryForm
 
-    def post(self, request, *args, **kwargs):
-        self.object = None
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
+    def get_form(self, form_class):
+        # Model.cleanでValidationを行うために
+        # ここで作者を設定している
+        # 議論参照
+        # https://github.com/kawazrepos/Kawaz3rd/pull/134
+        form = super().get_form(form_class)
         # 記事の作成者を自動的に指定
         form.instance.author = self.request.user
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
+        return form
 
 
 @permission_required('blogs.change_entry')
