fd9218b3af027ab247ef35ec10ce54ecb0a77e12
giginet <giginet.net@gmail.com>
2014-07-22 03:35:08 +0900

    Django commentフレームワークを追加した

--- src/kawaz/settings.py
+++ src/kawaz/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.comments',
     'rest_framework',
     'permission',
     'thumbnailfield',
