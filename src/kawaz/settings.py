a479c5ba9c81bce46055f726f1d15d17572f2185
giginet <giginet.net@gmail.com>
2014-07-27 09:54:33 +0900

    haystack + elasticsearchの導入

--- src/kawaz/settings.py
+++ src/kawaz/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django_comments',
+    'haystack',
     'rest_framework',
     'permission',
     'thumbnailfield',
