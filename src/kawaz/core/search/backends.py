3ca7d07ba93f07c48ab0acd258f217dc9c103fbb
giginet <giginet.net@gmail.com>
2014-07-27 13:19:18 +0900

    検索フォームの未入力をバリデーションするようにした

--- src/kawaz/core/search/backends.py
+++ src/kawaz/core/search/backends.py
@@ -54,8 +54,7 @@ class KuromojiElasticBackend(ElasticsearchSearchBackend):
             field_mapping = mapping[field_class.index_fieldname]
 
             if field_mapping['type'] == 'string' and field_class.indexed:
-                if not hasattr(field_class, 'facet_for') and not \
-                                  field_class.field_type in('ngram', 'edge_ngram'):
+                if not hasattr(field_class, 'facet_for') and not field_class.field_type in('ngram', 'edge_ngram'):
                     field_mapping['analyzer'] = self.DEFAULT_ANALYZER
             mapping.update({field_class.index_fieldname: field_mapping})
         return (content_field_name, mapping)
