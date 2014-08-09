9062601c647827491ee76630bbf6709d8dea97cf
giginet <giginet.net@gmail.com>
2014-08-09 17:59:56 +0900

    GroupListLinkWidget実装した

--- src/kawaz/apps/products/filters.py
+++ src/kawaz/apps/products/filters.py
@@ -1,21 +1,21 @@
 from django.utils.translation import ugettext as _
 import django_filters
 from django_filters import filters
-from django_filters import widgets
 from .models import Product
 from .models import Category, Platform
+from kawaz.core.filters.widgets import ListGroupLinkWidget
 
 
 class ProductFilter(django_filters.FilterSet):
     platforms = filters.ModelChoiceFilter(
         label=_('Platforms'),
         queryset=Platform.objects.all(),
-        widget=widgets.LinkWidget(choices=[('', _('All'))]))
+        widget=ListGroupLinkWidget(choices=[('', _('All'))]))
 
     categories = filters.ModelChoiceFilter(
         label=_('Categories'),
         queryset=Category.objects.all(),
-        widget=widgets.LinkWidget(choices=[('', _('All'))]))
+        widget=ListGroupLinkWidget(choices=[('', _('All'))]))
 
     class Meta:
         model = Product
