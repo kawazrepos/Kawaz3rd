568814f9b24529e18fcd003559183a380302e98a
giginet <giginet.net@gmail.com>
2014-09-21 15:36:11 +0900

    get_week_dayタグを実装した

--- src/kawaz/core/templatetags/tests/test_utils.py
+++ src/kawaz/core/templatetags/tests/test_utils.py
@@ -28,7 +28,7 @@ class ActiveTemplateTagTestCase(TestCase):
         render = t.render(c)
         self.assertEqual(render, 'active')
 
-    def test_active_contains_get_parametter(self):
+    def test_active_contains_get_parameter(self):
         """
         activeタグはGETパラメータの値も含む
         """
@@ -61,3 +61,59 @@ class ActiveTemplateTagTestCase(TestCase):
         })
         render = t.render(c)
         self.assertEqual(render, '')
+
+
+class GetWeekDayTemplateTagTestCase(TestCase):
+
+    def test_weekday(self):
+        '''
+        get_week_dayで平日だったときにweekdayを返す
+        '''
+        import datetime
+        t = Template(
+            """{% load utils %}"""
+            """{% get_week_day datetime %}"""
+        )
+        request = MagicMock()
+        c = Context({
+            'request':  request,
+            'datetime': datetime.datetime(2014, 9, 22)
+        })
+        render = t.render(c)
+        self.assertEqual(render, 'weekday')
+
+
+    def test_saturday(self):
+        '''
+        get_week_dayで土曜だったときにsaturdayを返す
+        '''
+        import datetime
+        t = Template(
+            """{% load utils %}"""
+            """{% get_week_day datetime %}"""
+        )
+        request = MagicMock()
+        c = Context({
+            'request':  request,
+            'datetime': datetime.datetime(2014, 9, 20)
+        })
+        render = t.render(c)
+        self.assertEqual(render, 'saturday')
+
+
+    def test_sunday(self):
+        '''
+        get_week_dayで平日だったときにweekdayを返す
+        '''
+        import datetime
+        t = Template(
+            """{% load utils %}"""
+            """{% get_week_day datetime %}"""
+        )
+        request = MagicMock()
+        c = Context({
+            'request':  request,
+            'datetime': datetime.datetime(2014, 9, 21)
+        })
+        render = t.render(c)
+        self.assertEqual(render, 'sunday')
