c3c0c96fd1689af921be66984ed93528e39f00c1
Λlisue <lambdalisue@hashnote.net>
2014-10-06 13:52:04 +0900

    Add alternative library of django.contrib.comments

--- src/kawaz/settings.py
+++ src/kawaz/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_comments',
     'rest_framework',
     'permission',
     'thumbnailfield',
