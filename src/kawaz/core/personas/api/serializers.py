b5bd8d5afef4d4cd9ac9dec4ce9cd425d0cc1bd8
giginet <giginet.net@gmail.com>
2014-10-09 20:37:10 +0900

    スターのAPIが元画像を引っ張ってきててパフォーマンス的に良くなかったので、smallをAPIから取れるようにした

--- src/kawaz/core/personas/api/serializers.py
+++ src/kawaz/core/personas/api/serializers.py
@@ -4,10 +4,15 @@ from ..models import Persona
 
 class PersonaSerializer(serializers.ModelSerializer):
     role = serializers.CharField(read_only=True)
+    small_avatar = serializers.CharField(source='get_small_avatar', read_only=True)
+    middle_avatar = serializers.CharField(source='get_middle_avatar', read_only=True)
+    large_avatar = serializers.CharField(source='get_large_avatar', read_only=True)
+    huge_avatar = serializers.CharField(source='get_huge_avatar', read_only=True)
 
     class Meta:
         model = Persona
         fields = (
             'id', 'nickname', 'quotes',
-            'avatar', 'gender', 'role',
+            'small_avatar', 'middle_avatar', 'large_avatar', 'huge_avatar',
+            'gender', 'role',
         )
