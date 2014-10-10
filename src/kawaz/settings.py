f2ecb0f3cc5d181f59d311a1a3938da1300824e2
Λlisue <lambdalisue@hashnote.net>
2014-10-11 05:13:30 +0900

    Move Profile.dev and improve documents

--- src/kawaz/settings.py
+++ src/kawaz/settings.py
@@ -7,28 +7,29 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
-
 import os
 import sys
+
 REPOSITORY_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'xd(wr812awpkuu4+7o)#ugb)*a0z!-m^an+m)%ly$l(ses8_g1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+SITE_ID = 1
+
+ROOT_URLCONF = 'kawaz.urls'
+WSGI_APPLICATION = 'kawaz.wsgi.application'
+AUTH_USER_MODEL = 'personas.Persona'
+
+TEST_RUNNER = 'kawaz.core.tests.runner.KawazDiscoverRunner'
+TESTING = False
 
 
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -77,81 +78,29 @@ MIDDLEWARE_CLASSES = (
     'roughpages.middleware.RoughpageFallbackMiddleware',
 )
 
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'permission.backends.PermissionBackend',
-)
-
-ROOT_URLCONF = 'kawaz.urls'
-
-WSGI_APPLICATION = 'kawaz.wsgi.application'
-
-# validation_on_save decorator (kawaz.core.db.decorators)
-# To disable automatical validation, set this variable to False
-VALIDATE_ON_SAVE_DISABLE = False
-
-AUTH_USER_MODEL = 'personas.Persona'
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(REPOSITORY_ROOT, 'db.sqlite3'),
-    }
-}
-
-THUMBNAIL_SIZE_PATTERNS = {
-    'huge': (288, 288,),
-    'large': (96, 96,),
-    'middle': (48, 48,),
-    'small': (24, 24,),
-}
-
-PRODUCT_THUMBNAIL_SIZE_PATTERNS = {
-    None : (32, 32),
-}
-
-ADVERTISEMENT_IMAGE_SIZE_PATTERNS = {
-    None : (32, 32),
-}
-
-SCREENSHOT_IMAGE_SIZE_PATTERNS = {
-    None : (32, 32),
-}
-
-# Permission presence check in DEBUG mode
-# from django-permission 0.5.3
-PERMISSION_CHECK_PERMISSION_PRESENCE = DEBUG
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
-
+from django.utils.translation import ugettext_lazy as _
+USE_I18N = True
 LANGUAGE_CODE = 'ja'
-
-gettext = lambda s: s
 LANGUAGES = (
-    ('en-us', gettext('English')),
-    ('ja', gettext('Japanese')),
+    ('en-us', _('English')),
+    ('ja', _('Japanese')),
 )
 
+USE_L10N = True
 LOCALE_PATHS = (
     os.path.join(REPOSITORY_ROOT, 'src', 'locale'),
 )
 
+USE_TZ = True
 TIME_ZONE = 'Asia/Tokyo'
 
-USE_I18N = True
 
-USE_L10N = True
-
-USE_TZ = True
-
+# Template
 TEMPLATE_DIRS = (
     os.path.join(REPOSITORY_ROOT, 'src', 'templates'),
 )
-
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
@@ -162,10 +111,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages",
     "django.core.context_processors.request"
 )
-
-LOGIN_URL = '/registration/login/'
-LOGOUT_URL = '/registration/logout/'
-LOGIN_REDIRECT_URL = '/'
 
 MEDIA_URL = '/storage/'
 MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'public', 'storage')
@@ -181,6 +126,57 @@ STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
 )
 
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(REPOSITORY_ROOT, 'db.sqlite3'),
+    }
+}
+# validation_on_save decorator (kawaz.core.db.decorators)
+# To disable automatical validation, set this variable to False
+VALIDATE_ON_SAVE_DISABLE = False
+
+
+# django-thumbnailfield
+THUMBNAIL_SIZE_PATTERNS = {
+    'huge': (288, 288,),
+    'large': (96, 96,),
+    'middle': (48, 48,),
+    'small': (24, 24,),
+}
+PRODUCT_THUMBNAIL_SIZE_PATTERNS = {
+    None: (32, 32),
+}
+ADVERTISEMENT_IMAGE_SIZE_PATTERNS = {
+    None: (32, 32),
+}
+SCREENSHOT_IMAGE_SIZE_PATTERNS = {
+    None: (32, 32),
+}
+
+# django-permission
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'permission.backends.PermissionBackend',
+)
+# Permission presence check in DEBUG mode
+# from django-permission 0.5.3
+PERMISSION_CHECK_PERMISSION_PRESENCE = DEBUG
+
+
+# django-inspectional-registration
+REGISTRATION_SUPPLEMENT_CLASS = (
+    'kawaz.core.registrations.models.RegistrationSupplement')
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_DJANGO_AUTH_URLS_ENABLE = False
+LOGIN_URL = '/registration/login/'
+LOGOUT_URL = '/registration/logout/'
+LOGIN_REDIRECT_URL = '/'
+
+
+# django-compressor
 COMPRESS_ENABLED = not DEBUG
 COMPRESS_OUTPUT_DIR = ''
 COMPRESS_PRECOMPILERS = (
@@ -188,26 +184,15 @@ COMPRESS_PRECOMPILERS = (
     ('text/less', 'lessc {infile} {outfile}'),
 )
 
-# inspectional-registration
-REGISTRATION_SUPPLEMENT_CLASS = (
-    'kawaz.core.registrations.models.RegistrationSupplement')
-ACCOUNT_ACTIVATION_DAYS = 7
-REGISTRATION_DJANGO_AUTH_URLS_ENABLE = False
-
-# rest-framework
+# django-rest-framework
 DEFAULT_RENDERER_CLASSES = (
     'rest_framework.renderers.JSONRenderer',
 )
 
-TEST_RUNNER = 'kawaz.core.tests.runner.KawazDiscoverRunner'
-TESTING = False
-
-RECENT_ACTIVITY_FEED_URL = 'http://kawazinfo.hateblo.jp/rss'
+# kawaz.apps.activities.contrib.hatenablog
 ACTIVITIES_HATENABLOG_FEED_URL = 'http://kawazinfo.hateblo.jp/rss'
 
-SITE_ID = 1
-
-# Google Calendar Synchronize
+# kawaz.apps.events.gcal
 GCAL_CALENDAR_ID = (
     # DEBUG用カレンダー
     'kawaz.org_u41faouova38rcoh8eaimbg42c@group.calendar.google.com'
@@ -224,6 +209,7 @@ GCAL_CREDENTIALS = os.path.join(
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 CRISPY_ALLOWED_TEMPLATE_PACKS = ('bootstrap3', 'crispy')
 
+
 if DEBUG:
     # テスト時のRuntimeWarningをexceptionにしている
     # https://docs.djangoproject.com/en/dev/topics/i18n/timezones/#code
@@ -232,16 +218,14 @@ if DEBUG:
         'error', r"DateTimeField .* received a naive datetime",
         RuntimeWarning, r'django\.db\.models\.fields')
 
-
-# 環境依存の設定（デプロイサーバー固有の設定など）を読み込む
-LOCAL_SETTINGS_LOADED = False
-
 # DjangoのMessageをBootstrap3に適応させている
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
+# 環境依存の設定（デプロイサーバー固有の設定など）を読み込む
+LOCAL_SETTINGS_LOADED = False
 try:
     from .local_settings import *
 except ImportError:
