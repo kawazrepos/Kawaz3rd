de7006f7de80ec56f849b7dd75d8284c76b87ccd
giginet <giginet.net@gmail.com>
2014-10-26 16:44:14 +0900

    kawaz.core.activitiesのテストモデル廃止した

--- src/kawaz/core/activities/tests/test_views.py
+++ src/kawaz/core/activities/tests/test_views.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
+from activities.models import Activity
 from activities.registry import registry
 from activities.mediator import ActivityMediator
-from .models import KawazActivitiesTestModelA, KawazActivitiesTestModelB
+from kawaz.core.personas.models import Persona
+from kawaz.core.personas.tests.factories import PersonaFactory
 from kawaz.core.activities.tests.factories import ActivityFactory
 
 __author__ = 'giginet'
@@ -11,8 +13,7 @@ __author__ = 'giginet'
 class ActivityViewTestCase(TestCase):
 
     def setUp(self):
-        registry.register(KawazActivitiesTestModelA, ActivityMediator())
-        registry.register(KawazActivitiesTestModelB, ActivityMediator())
+        registry.register(Persona, ActivityMediator())
 
     def test_activities_activity_list_url(self):
         """
@@ -25,7 +26,7 @@ class ActivityViewTestCase(TestCase):
         10件ずつActivityを取得できる
         """
         for i in range(15):
-            model = KawazActivitiesTestModelA.objects.create(text="aaa")
+            PersonaFactory()
         r = self.client.get('/activities/')
         self.assertEqual(len(r.context['object_list']), 10)
 
@@ -38,11 +39,13 @@ class ActivityViewTestCase(TestCase):
         """
          type=wallのとき、latestsの物だけを10件取得できる
         """
-        model0 = KawazActivitiesTestModelA.objects.create(text="aaa")
-        model1 = KawazActivitiesTestModelB.objects.create(text="aaa")
+        for i in range(5):
+            persona = PersonaFactory()
+            persona.nickname = 'user{}'.format(i)
+            persona.save()
 
         r = self.client.get('/activities/?type=wall')
-        self.assertEqual(len(r.context['object_list']), 2)
+        self.assertEqual(len(r.context['object_list']), 5)
 
         r = self.client.get('/activities/')
-        self.assertEqual(len(r.context['object_list']), 6)
+        self.assertEqual(len(r.context['object_list']), 10)
