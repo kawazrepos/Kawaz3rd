1263e9a7fa50b6362e6cb5775cc68b1574705d88
Λlisue <lambdalisue@hashnote.net>
2014-10-26 15:33:17 +0900

    expr テンプレートタグの追加

---
+++ src/kawaz/core/templatetags/tests/test_expr.py
@@ -0,0 +1,53 @@
+from django.test import TestCase
+from django.template import Template, Context
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+from ..templatetags.markdown import markdown
+
+
+class ExprTemplateTagTestCase(TestCase):
+
+    def test_expr_render(self):
+        """
+        expr タグを利用して計算結果を描画可能
+        """
+        t = Template(
+            """{% load expr %}"""
+            """{% expr 1 + 1 %}"""
+        )
+        c = Context()
+        render = t.render(c)
+        self.assertTrue('2' in render)
+
+        t = Template(
+            """{% load expr %}"""
+            """{% expr 1 + var1 %}"""
+        )
+        c = Context({'var1': 10})
+        render = t.render(c)
+        self.assertTrue('11' in render)
+
+    def test_expr_render(self):
+        """
+        expr タグを利用して計算結果を代入可能
+        """
+        t = Template(
+            """{% load expr %}"""
+            """{% expr 1 + 1 as result %}"""
+        )
+        c = Context()
+        render = t.render(c)
+        self.assertFalse('2' in render)
+        self.assertTrue('result' in c)
+        self.assertEqual(c['result'], 2)
+
+        t = Template(
+            """{% load expr %}"""
+            """{% expr 1 + var1 as result %}"""
+        )
+        c = Context({'var1': 10})
+        render = t.render(c)
+        self.assertFalse('11' in render)
+        self.assertTrue('result' in c)
+        self.assertEqual(c['result'], 11)
+
