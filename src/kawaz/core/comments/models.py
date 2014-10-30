5dc9c2e57d8939879846ed6f8ec86e265555552b
giginet <giginet.net@gmail.com>
2014-10-30 22:48:32 +0900

    コメント用のPermissionLogicを実装して正しく削除できるようにした

--- src/kawaz/core/comments/models.py
+++ src/kawaz/core/comments/models.py
@@ -5,3 +5,7 @@ from django_comments.models import Comment
 from .activity import CommentActivityMediator
 from activities.registry import registry
 registry.register(Comment, CommentActivityMediator())
+
+from permission import add_permission_logic
+from .perms import CommentPermissionLogic
+add_permission_logic(Comment, CommentPermissionLogic())
