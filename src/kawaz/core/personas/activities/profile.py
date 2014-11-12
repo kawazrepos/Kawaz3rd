46552f1a64a64c9914ae4b7d50b8fe8b14d97971
giginet <giginet.net@gmail.com>
2014-11-12 17:28:06 +0900

    Personaの通知周りで500が出てた問題を修正。プロフィールの更新通知がおかしかったのも修正

--- src/kawaz/core/personas/activities/profile.py
+++ src/kawaz/core/personas/activities/profile.py
@@ -15,39 +15,9 @@ class ProfileActivityMediator(ActivityMediator):
             activity.content_type = ct
             activity.object_id = pk
             activity.status = 'profile_updated'
-
-            previous = activity.previous.snapshot
-            is_created = lambda x: (
-                not getattr(previous, x, None) and
-                getattr(instance, x)
-            )
-            is_updated = lambda x: (
-                getattr(previous, x, None) and
-                getattr(instance, x) and
-                getattr(previous, x) != getattr(instance, x)
-            )
-            is_deleted = lambda x: (
-                getattr(previous, x, None) and
-                not getattr(instance, x)
-            )
-            remarks = []
-            attributes = (
-                'remarks',
-                'birthday',
-                'place',
-                'url'
-            )
-            for attribute in attributes:
-                if is_created(attribute):
-                    remarks.append(attribute + '_created')
-                elif is_updated(attribute):
-                    remarks.append(attribute + '_updated')
-                elif is_deleted(attribute):
-                    remarks.append(attribute + '_deleted')
-            if not remarks:
-                # 通知が必要な変更ではないため通知しない
-                return None
-            activity.remarks = "\n".join(remarks)
+            # snapshotはPersonaのものになるため
+            # Profileの情報を取り出せない
+            # そのため、フラグをremarksに設定していない
         return activity
 
 
