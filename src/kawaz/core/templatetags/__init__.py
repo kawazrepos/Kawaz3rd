b8d23d7815dc3b2160b404da1204f64a6cc4e327
lambdalisue <lambdalisue@hashnote.net>
2014-11-21 01:07:37 +0900

    すべての `parser` を `kfm` に置き換え

--- src/kawaz/core/templatetags/__init__.py
+++ src/kawaz/core/templatetags/__init__.py
@@ -2,3 +2,4 @@
 from django.template import add_to_builtins
 add_to_builtins('kawaz.core.templatetags.templatetags.markdown')
 add_to_builtins('kawaz.core.templatetags.templatetags.expr')
+add_to_builtins('kawaz.apps.kfm.templatetags.kfm')
