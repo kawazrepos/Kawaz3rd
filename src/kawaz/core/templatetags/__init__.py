9b539cecc4ccd89bf1d23dce304d7c53d2b07174
lambdalisue <lambdalisue@hashnote.net>
2014-11-21 01:21:10 +0900

    廃止されたテンプレートタグ関係のコードとファイルを削除

--- src/kawaz/core/templatetags/__init__.py
+++ src/kawaz/core/templatetags/__init__.py
@@ -1,5 +1,4 @@
 # 一部template tagをbuilt in filter化します
 from django.template import add_to_builtins
-add_to_builtins('kawaz.core.templatetags.templatetags.markdown')
 add_to_builtins('kawaz.core.templatetags.templatetags.expr')
 add_to_builtins('kawaz.apps.kfm.templatetags.kfm')
