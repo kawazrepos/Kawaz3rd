2a8bc2b09b90aeac7fcb5829c4b0a06f785b525d
lambdalisue <lambdalisue@hashnote.net>
2014-11-21 01:44:52 +0900

    strip_newlines フィルタを実装

---
+++ src/kawaz/core/templatetags/tests/test_strip_newlines.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+"""
+"""
+__author__ = 'Alisue <lambdalisue@hashnote.net>'
+from django.test import TestCase
+from django.template import Template, Context
+
+
+class StripNewlinesTemplateFilterTestCase(TestCase):
+
+    def test_strip_newlines(self):
+        """strip_newlinesフィルタで改行文字の除去が可能"""
+        t = Template(
+            """{% load strip_newlines %}"""
+            """{{ body|strip_newlines }}"""
+        )
+        r = t.render(Context(dict(
+            body="foo\rbar\nhoge\r\npiyo",
+        )))
+        self.assertIn('foobarhogepiyo', r)
+
+    def test_strip_newlines(self):
+        """strip_newlinesフィルタで改行文字の置換が可能"""
+        t = Template(
+            """{% load strip_newlines %}"""
+            """{{ body|strip_newlines:'@' }}"""
+        )
+        r = t.render(Context(dict(
+            body="foo\rbar\nhoge\r\npiyo\n\npiyo",
+        )))
+        self.assertIn('foo@bar@hoge@piyo@@piyo', r)
