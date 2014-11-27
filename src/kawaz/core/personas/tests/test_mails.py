61ceab833646438d5984b8f6854372b03ffb862c
giginet <giginet.net@gmail.com>
2014-11-27 16:31:30 +0900

    パスワードリセットメールが空白な問題を修正 closes #703

--- src/kawaz/core/personas/tests/test_mails.py
+++ src/kawaz/core/personas/tests/test_mails.py
@@ -1,5 +1,9 @@
+from unittest.mock import MagicMock
+from django.template import Context
 from django.test import TestCase
 from django.template.loader import render_to_string
+from django.utils.http import int_to_base36
+
 
 class PersonaPasswordResetMailTemplateTestCase(TestCase):
     def test_email_subject_parse(self):
@@ -7,12 +11,22 @@ class PersonaPasswordResetMailTemplateTestCase(TestCase):
         registration/password_reset_email_subject.txtを正しくparseできる
         """
         rendered = render_to_string('registration/password_reset_subject.txt')
-        self.assertIsNotNone(rendered)
+        self.assertTrue(rendered)
 
     def test_email_parse(self):
         """
         registration/password_reset_email.htmlを正しくparseできる
         """
-        rendered = render_to_string('registration/password_reset_email.html')
-        self.assertIsNotNone(rendered)
+        user = MagicMock()
+        c = Context({
+            'email': 'webmaster@kawaz.org',
+            'domain': 'www.kawaz.org',
+            'site_name': 'Kawaz',
+            'uid': int_to_base36(1),
+            'user': user,
+            'token': "hogehogehogehoge",
+            'protocol': 'http',
+        })
+        rendered = render_to_string('registration/password_reset_email.html', c)
+        self.assertTrue(rendered)
 
