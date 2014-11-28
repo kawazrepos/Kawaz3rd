c73d4be813ada97f29161e426811b41562b0e33c
giginet <giginet.net@gmail.com>
2014-11-28 22:01:13 +0900

    ニコ動、YouTubeのパーサー、コメントおかしいところ直した

--- src/kawaz/apps/kfm/extras/youtube.py
+++ src/kawaz/apps/kfm/extras/youtube.py
@@ -47,7 +47,7 @@ def parse_youtube_urls(value, responsive=False, width=None, height=None):
 
     def repl(m):
         if is_quoated(m.string, m.start(), m.end()):
-            # ', ", [] に囲まれているため置換を行わない
+            # ', "に囲まれているため置換を行わない
             return m.group()
         params = dict(
             video_id=m.group('id'),
