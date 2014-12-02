d7f242302453dbf833526630e83f885b495d9131
giginet <giginet.net@gmail.com>
2014-12-02 22:57:16 +0900

    ブログカテゴリの生成をdjango-rest-framework3.0対応した

--- src/kawaz/apps/blogs/api/views.py
+++ src/kawaz/apps/blogs/api/views.py
@@ -2,7 +2,10 @@
 #
 # created by giginet on 2014/7/19
 #
+
 __author__ = 'giginet'
+from rest_framework import status
+from rest_framework.response import Response
 from kawaz.api import mixins
 from kawaz.api.views import KawazGenericViewSet
 from .serializers import CategorySerializer
@@ -18,3 +21,16 @@ class CategoryViewSet(mixins.CreateModelMixin,
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     author_field_name = 'author'
+
+    def create(self, request, *args, **kwargs):
+        label = request.data['label']
+        author = request.user
+        try:
+            category = Category.objects.create(label=label, author=author)
+            serializer = self.get_serializer(instance=category)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        except:
+            serializer = self.get_serializer(data=request.data)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST, headers=headers)
