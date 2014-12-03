5d1c8d0b74fc363b6fa3cbc2cb464edf0a8f7548
giginet <giginet.net@gmail.com>
2014-12-03 20:28:53 +0900

    プラットフォーム選択ボタンにアイコンを表示した closes #632

--- src/kawaz/apps/products/filters.py
+++ src/kawaz/apps/products/filters.py
@@ -1,3 +1,6 @@
+from django.db.models import BLANK_CHOICE_DASH
+from django.utils.encoding import force_text
+from django.utils.http import urlencode
 from django.utils.translation import ugettext_lazy as _
 import django_filters
 from django_filters import filters
@@ -6,11 +9,47 @@ from .models import Category, Platform
 from kawaz.core.filters.widgets import ListGroupLinkWidget
 
 
+class PlatformListGroupLinkWidget(ListGroupLinkWidget):
+    """
+    プラットフォームアイコンを一覧に出すWidget
+    """
+    def __init__(self, attrs=None, choices=()):
+        super().__init__(attrs, choices)
+        platforms = Platform.objects.all()
+        self.urls = {str(platform.pk): platform.icon.url for platform in platforms}
+
+    def render_option(self, name, selected_choices,
+                          option_value, option_label):
+        option_value = force_text(option_value)
+        if option_label == BLANK_CHOICE_DASH[0][1]:
+            option_label = _("All")
+        data = self.data.copy()
+        data[name] = option_value
+        selected = data == self.data or option_value in selected_choices
+
+        icon = ''
+        if option_value in self.urls:
+            icon = self.urls[option_value]
+        try:
+            url = data.urlencode()
+        except AttributeError:
+            url = urlencode(data)
+        return self.option_string() % {
+            'attrs': selected and ' class="list-group-item active"' or '',
+            'query_string': url,
+            'label': force_text(option_label),
+            'icon': icon
+        }
+
+    def option_string(self):
+        return '<a%(attrs)s href="?%(query_string)s" class="list-group-item"><img class="platform-icon" src="%(icon)s">%(label)s</a>'
+
+
 class ProductFilter(django_filters.FilterSet):
     platforms = filters.ModelChoiceFilter(
         label=_('Platforms'),
         queryset=Platform.objects.all(),
-        widget=ListGroupLinkWidget(choices=[('', _('All'))]))
+        widget=PlatformListGroupLinkWidget(choices=[('', _('All'))]))
 
     categories = filters.ModelChoiceFilter(
         label=_('Categories'),
