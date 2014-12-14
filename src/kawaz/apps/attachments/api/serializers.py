5016c45c3e2766cf5bfdac059a8f9b67cbc3dd05
giginet <giginet.net@gmail.com>
2014-12-15 07:53:22 +0900

    djangorestframework 3.0.1に対応

--- src/kawaz/apps/attachments/api/serializers.py
+++ src/kawaz/apps/attachments/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from kawaz.core.personas.models import Persona
 from ..models import Material
 from kawaz.core.personas.api.serializers import PersonaSerializer
 
@@ -12,3 +13,11 @@ class MaterialSerializer(serializers.ModelSerializer):
         fields = (
             'content_file', 'author', 'slug', 'ip_address', 'tag'
         )
+
+    def create(self, validated_data):
+        # django-rest-framework 3.0.1から明示的に書かなきゃいけなくなった
+        # http://www.django-rest-framework.org/api-guide/serializers/#writable-nested-representations
+        author_data = validated_data.pop('author')
+        validated_data['author'] = Persona.objects.get(pk=author_data.pk)
+        material = Material.objects.create(**validated_data)
+        return material
--- src/kawaz/apps/blogs/api/serializers.py
+++ src/kawaz/apps/blogs/api/serializers.py
@@ -2,6 +2,8 @@
 #
 # created by giginet on 2014/7/19
 #
+from kawaz.core.personas.models import Persona
+
 __author__ = 'giginet'
 from rest_framework import serializers
 from kawaz.core.personas.api.serializers import PersonaSerializer
@@ -21,3 +23,11 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = (
             'id', 'author', 'label'
         )
+
+    def create(self, validated_data):
+        # django-rest-framework 3.0.1から明示的に書かなきゃいけなくなった
+        # http://www.django-rest-framework.org/api-guide/serializers/#writable-nested-representations
+        author_data = validated_data.pop('author')
+        validated_data['author'] = Persona.objects.get(pk=author_data.pk)
+        category = Category.objects.create(**validated_data)
+        return category
