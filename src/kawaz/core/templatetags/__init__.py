7161f8965a4022622bdaf9018b369a72f1b38e46
giginet <giginet.net@gmail.com>
2015-04-18 23:10:16 +0900

    add_to_functionの場所が変わっていたので修正

--- src/kawaz/core/templatetags/__init__.py
+++ src/kawaz/core/templatetags/__init__.py
@@ -1,4 +1,4 @@
 # 一部template tagをbuilt in filter化します
-from django.template import add_to_builtins
+from django.template.base import add_to_builtins
 add_to_builtins('kawaz.core.templatetags.templatetags.expr')
 add_to_builtins('kawaz.apps.kfm.templatetags.kfm')
