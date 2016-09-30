8eb0a41b1aa3a3715a83ccd6fba9dc89ac15b6e1
giginet <giginet.net@gmail.com>
2016-10-01 02:15:52 +0900

    Remove debug context_processor

--- src/kawaz/settings.py
+++ src/kawaz/settings.py
@@ -91,7 +91,6 @@ TEMPLATES = [
                 'kawaz.apps.kfm.templatetags.kfm',
             ],
             'context_processors': [
-                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
