c02a93825bce97114cea4cc3bcc139c8e9703871
giginet <giginet.net@gmail.com>
2016-10-01 22:20:02 +0900

    Use standard importlib

--- src/kawaz/core/management/management/commands/start_livereload.py
+++ src/kawaz/core/management/management/commands/start_livereload.py
@@ -5,7 +5,7 @@
 import os
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
-from django.utils.importlib import import_module
+from importlib import import_module
 from livereload import Server
 
 
