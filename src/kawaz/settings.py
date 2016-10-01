0e5c73cc08c06b1e6afb1e6cefbe88d4aeb33bfb
giginet <giginet.net@gmail.com>
2016-10-01 22:19:50 +0900

    Add missing context_processor

--- src/kawaz/settings.py
+++ src/kawaz/settings.py
@@ -90,6 +90,10 @@ TEMPLATES = [
                 'kawaz.apps.kfm.templatetags.kfm',
             ],
             'context_processors': [
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
