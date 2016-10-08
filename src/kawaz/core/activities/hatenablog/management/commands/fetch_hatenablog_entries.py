8c5ee41fb9821bf319fb7cea3d366c7b3a8298c9
giginet <giginet.net@gmail.com>
2016-10-09 02:02:22 +0900

    Fix code format

--- src/kawaz/core/activities/hatenablog/management/commands/fetch_hatenablog_entries.py
+++ src/kawaz/core/activities/hatenablog/management/commands/fetch_hatenablog_entries.py
@@ -1,4 +1,3 @@
-from optparse import make_option
 from django.core.management.base import BaseCommand
 from ...conf import settings
 from ...models import HatenablogEntry
@@ -13,23 +12,23 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--clear', action='store_true', default=False,
-                help=("Clear entries in database to synchronize "
-                    "entries in database and actual."))
+                            help=("Clear entries in database to synchronize "
+                                  "entries in database and actual."))
         parser.add_argument('--url',
-                default=settings.ACTIVITIES_HATENABLOG_FEED_URL,
-                help=("Specify a url of hatenablog which will be parsed. "
-                    "The default url is '{}'.").format(URL))
+                            default=settings.ACTIVITIES_HATENABLOG_FEED_URL,
+                            help=("Specify a url of hatenablog which will be parsed. "
+                                  "The default url is '{}'.").format(URL))
 
     def handle(self, *args, **options):
         verbosity = int(options.get('verbosity'))
         if options.get('clear'):
             if verbosity > 0:
-                print(("Clearing hatenablog entries in database..."))
+                print("Clearing hatenablog entries in database...")
             HatenablogEntry.objects.clear()
 
         if verbosity > 0:
-            print(("Fetching hatenablog entries from '{}'...").format(
-                options.get('url'),
+            print("Fetching hatenablog entries from '{}'...".format(
+                    options.get('url'),
             ))
         scraper = HatenablogFeedScraper(url=options.get('url'),
                                         verbose=verbosity > 0)
@@ -38,5 +37,5 @@ class Command(BaseCommand):
         if verbosity > 0:
             print("*" * 80 + "\n")
             print(("{} hatenablog entries are created.\n"
-                "{} hatenablog entries are updated.\n").format(ncreated, nupdated))
+                   "{} hatenablog entries are updated.\n").format(ncreated, nupdated))
             print("*" * 80)
