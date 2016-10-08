e689c8aee28b543594e09386f6cf1075d42fe818
giginet <giginet.net@gmail.com>
2016-10-09 02:30:15 +0900

    Suppress Warning on testing

--- src/lib/django-activities/activities/notifiers/oauth/base.py
+++ src/lib/django-activities/activities/notifiers/oauth/base.py
@@ -74,7 +74,7 @@ class OAuth1ActivityNotifier(ActivityNotifierBase):
         The return value is a dictionary to initialize a
         requests_oauthlib.OAuth1
         """
-        if not os.path.exists(filename):
+        if not os.path.exists(filename) and not settings.TESTING:
             warnings.warn((
                 "'{}' is not found. Confirm if the file exists."
             ).format(filename), ImproperlyConfiguredWarning)
