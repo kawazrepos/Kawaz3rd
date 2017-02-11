0c8281ccd2440847103f11349d1672b9d33c4fbb
giginet <giginet.net@gmail.com>
2017-02-11 23:54:58 +0900

    Use mysqlclient

--- src/kawaz/__init__.py
+++ src/kawaz/__init__.py
@@ -1,13 +1,2 @@
-#! -*- coding: utf-8 -*-
-#
-#
-#
-
 __version__ = (0, 1, 0)
 VERSION = ".".join(map(str, __version__))
-
-try:
-    import pymysql
-    pymysql.install_as_MySQLdb()
-except:
-    pass
